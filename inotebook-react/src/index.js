5e582f4a9a866faeb02ba1b2e8cebe2aa6d03301
Arpit Kumar <Arpit7.Kumar@ril.com>
2023-07-24 14:41:35 +0530

    Added redux persist

--- inotebook-react/src/index.js
+++ inotebook-react/src/index.js
@@ -5,12 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import Store from './redux/store/Store';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+let persister = persistStore(Store);
+
 root.render(
   <React.StrictMode>
     <Provider store={Store}>
-      <App />
+      <PersistGate persistor={persister}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- inotebook-react/src/redux/store/Store.js
+++ inotebook-react/src/redux/store/Store.js
@@ -1,10 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AuthSlice from "../slices/AuthSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+
+const persistConfig = {
+    key: "root",
+    version: 1,
+    storage
+}
+
+const reducer = combineReducers({
+    auth: AuthSlice
+})
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 const Store = configureStore({
-    reducer: {
-        auth: AuthSlice
-    }
+    reducer: persistedReducer
 })
 
 export default Store;
